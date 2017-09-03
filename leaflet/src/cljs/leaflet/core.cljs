1d69fbc197b81af9c21e7b97b2279feacc10ee92
Jim Gumbley <jgumbley@thoughtworks.com>
2017-09-03 10:44:36 +0100

    move tile url into a function

--- leaflet/src/cljs/leaflet/core.cljs
+++ leaflet/src/cljs/leaflet/core.cljs
@@ -5,9 +5,12 @@
   [:div#mapdiv {:style {:height "100vh"
                         :width "100vw"}}])
 
+(def tile-url
+  "http://{s}.tile2.openclemap.org/transport/{z}/{x}/{y}.png")
+
 (defn home-did-mount []
   (let [leaflet (.setView (.map js/L "mapdiv") #js [52.53107999999999 -1.9730885000000171] 11)]
-    (.addTo (.tileLayer js/L "http://{s}.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png"
+    (.addTo (.tileLayer js/L tile-url
                         (clj->js {:attribution "Map data &copy; [...]"
                                   :maxZoom 18
                                   }))
